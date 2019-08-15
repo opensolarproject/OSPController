56c79505356953eb22c72c8fbd2639f5428b18ba
Tim O'Brien <timo@t413.com>
2019-08-15 08:53:28 -0700

    Move to platform.io .. woo? I want unit tests.

---
+++ lib/MPPTLib/solar.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <Arduino.h>
+#include <functional>
+#include <map>
+#include <list>
+
+class System {
+public:
+  System();
+};
+
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <solar.h>
+
+System system;
+
+int main(char** argv, int argc) {
+    system.run();
+}
