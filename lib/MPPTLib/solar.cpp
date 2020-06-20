74bb88040c6578ac869d1b6c3ba2e322225a09c5
Tim O'Brien <timo@t413.com>
2020-06-19 17:18:09 -0700

    Adds /update page to update the ESP firmware via web 🌍🔥

--- lib/MPPTLib/solar.cpp
+++ lib/MPPTLib/solar.cpp
@@ -24,6 +24,7 @@ uint32_t lastPSUpdate_ = 0, lastPSUSuccess_ = 0;
 uint32_t nextAutoSweep_ = 0, lastAutoSweep_ = 0;
 double newDesiredCurr_ = 0;
 String logme;
+extern const String updateIndex;
 
 void Solar::setup() {
   Serial.begin(115200);
@@ -78,10 +79,14 @@ void Solar::setup() {
     if (! ret.length()) ret = pub_.toJson();
     server_.send(200, "application/json", ret.c_str());
   });
+
+  server_.on("/update", HTTP_GET, [this](){
+    server_.sendHeader("Connection", "close");
+    server_.send(200, "text/html", updateIndex);
+  });
   server_.on("/update", HTTP_POST, [this](){
     server_.sendHeader("Connection", "close");
     server_.send(200, "text/plain", (Update.hasError())?"FAIL":"OK");
-    // esp_wifi_wps_disable();
     ESP.restart();
   },[this](){
     HTTPUpload& upload = server_.upload();
@@ -90,11 +95,10 @@ void Solar::setup() {
       if (!Update.begin(UPDATE_SIZE_UNKNOWN))//start with max available size
         Update.printError(Serial);
     } else if (upload.status == UPLOAD_FILE_WRITE){
-      /* flashing firmware to ESP*/
       if (Update.write(upload.buf, upload.currentSize) != upload.currentSize)
         Update.printError(Serial);
     } else if(upload.status == UPLOAD_FILE_END){
-      if(Update.end(true)) //true to set the size to the current progress
+      if(Update.end(true))
         log(str("Update Success: %u\nRebooting...\n", upload.totalSize));
       else Update.printError(Serial);
     }
@@ -430,3 +434,48 @@ void Solar::setState(const String state) {
   state_ = state;
 }
 
+//page styling
+const String style =
+"<style>#file-input,input{width:100%;height:44px;border-radius:4px;margin:10px auto;font-size:15px}"
+"input{background:#f1f1f1;border:0;padding:0 15px}body{background:#3498db;font-family:sans-serif;font-size:14px;color:#777}"
+"#file-input{padding:0;border:1px solid #ddd;line-height:44px;text-align:left;display:block;cursor:pointer}"
+"#bar,#prgbar{background-color:#f1f1f1;border-radius:10px}#bar{background-color:#3498db;width:0%;height:10px}"
+"form{background:#fff;max-width:258px;margin:75px auto;padding:30px;border-radius:5px;text-align:center}"
+".btn{background:#3498db;color:#fff;cursor:pointer}</style>";
+
+// Update page
+const String updateIndex =
+"<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>"
+"<form method='POST' action='#' enctype='multipart/form-data' id='upload_form'>"
+"<input type='file' name='update' id='file' onchange='sub(this)' style=display:none>"
+"<label id='file-input' for='file'>   Choose file...</label>"
+"<input type='submit' class=btn value='Update'>"
+"<br><br>"
+"<div id='prg'></div>"
+"<br><div id='prgbar'><div id='bar'></div></div><br></form>"
+"<script>"
+"function sub(obj){"
+"var fileName = obj.value.split('\\\\');"
+"document.getElementById('file-input').innerHTML = '   '+ fileName[fileName.length-1];"
+"};"
+"$('form').submit(function(e){"
+"e.preventDefault();"
+"var form = $('#upload_form')[0];"
+"var data = new FormData(form);"
+"$.ajax({ url: '/update', type: 'POST', data: data, contentType: false, processData:false,"
+"xhr: function() {"
+"var xhr = new window.XMLHttpRequest();"
+"xhr.upload.addEventListener('progress', function(evt) {"
+"if (evt.lengthComputable) {"
+"var per = evt.loaded / evt.total;"
+"$('#prg').html('progress: ' + Math.round(per*100) + '%');"
+"$('#bar').css('width',Math.round(per*100) + '%');"
+"}"
+"}, false);"
+"return xhr;"
+"},"
+"success:function(d, s) { console.log('success!') },"
+"error: function (a, b, c) { }"
+"});"
+"});"
+"</script>" + style;
